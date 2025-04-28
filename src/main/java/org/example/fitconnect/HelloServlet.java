eee90abdee9073802cf277feeef50b0132a4c18c
Arman Bhatia <arman.bhatia.1407@gmail.com>
2025-04-28 02:37:40 -0400

    Server tomcat 10 working

--- src/main/java/org/example/fitconnect/HelloServlet.java
+++ src/main/java/org/example/fitconnect/HelloServlet.java
@@ -4,12 +4,12 @@ import java.io.*;
 import jakarta.servlet.http.*;
 import jakarta.servlet.annotation.*;
 
-@WebServlet(name = "helloServlet", value = "/hello-servlet")
+@WebServlet("/hello-servlet")
 public class HelloServlet extends HttpServlet {
     private String message;
 
     public void init() {
-        message = "Hello World!";
+        message = "Hello Arman!";
     }
 
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
